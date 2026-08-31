5581c7b3d4a423d324eae06e0b136a5fe04dddaf
BeZide93 <r.wolter93@gmx.de>
2026-08-31 19:03:35 +0200

    Restore Dominion Rod power in Boss Rush

--- src/new_save_modes.cpp
+++ src/new_save_modes.cpp
@@ -1194,6 +1194,7 @@ void set_saved_dungeon_switch(int saveTable, int sw, bool enabled) {
 
 void ensure_bossrush_story_state() {
     dComIfGs_onEventBit(dSv_event_flag_c::F_0250);
+    dComIfGs_onEventBit(dSv_event_flag_c::F_0302);
     dComIfGs_onEventBit(dSv_event_flag_c::M_010);
 }
 
